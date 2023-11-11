f846285603bb0594e7d7ac45dfcb4583f140d5d8
Lázaro Albuquerque <33807434+lzralbu@users.noreply.github.com>
2023-11-10 22:01:54 -0400

    Added LICENSE, README and screenshots

---
+++ README.md
@@ -0,0 +1,11 @@
+# Computer graphics from scratch
+
+Implementation of the two renderers described in the book "Computer Graphics from Scratch by Gabriel Gambetta".
+
+For compiling you need CMake, a C99 compiler and Raylib.
+
+Some screenshots:
+
+![raytracing](raytracing-1.png)
+![raytracing](raytracing-2.png)
+![raytracing](raytracing-3.png)
--- src/main.c
+++ src/main.c
@@ -111,32 +111,24 @@ vector3_t vector3_scale(vector3_t v, double s) {
     return (vector3_t){ .x = v.x * s, .y = v.y * s, .z = v.z * s };
 }
 
-vector3_t vector3_negate(vector3_t v) {
-    return vector3_scale(v, -1.0);
-}
+vector3_t vector3_negate(vector3_t v) { return vector3_scale(v, -1.0); }
 
 double vector3_dot(vector3_t v1, vector3_t v2) {
     return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
 }
 
-double vector3_length(vector3_t v) {
-    return sqrt(vector3_dot(v, v));
-}
+double vector3_length(vector3_t v) { return sqrt(vector3_dot(v, v)); }
 
-vector3_t vector3_normalize(vector3_t v) {
-    return vector3_scale(v, 1 / vector3_length(v));
-}
+vector3_t vector3_normalize(vector3_t v) { return vector3_scale(v, 1 / vector3_length(v)); }
 
 vector3_t vector3_reflect(vector3_t v, vector3_t normal) {
     return vector3_subtract(vector3_scale(normal, 2 * vector3_dot(v, normal)), v);
 }
 
 vector3_t vector3_transform(vector3_t v, matrix3_t transform) {
-    return (vector3_t){
-        .x = transform.a11 * v.x + transform.a12 * v.y + transform.a13 * v.z,
-        .y = transform.a21 * v.x + transform.a22 * v.y + transform.a23 * v.z,
-        .z = transform.a31 * v.x + transform.a32 * v.y + transform.a33 * v.z
-    };
+    return (vector3_t){ .x = transform.a11 * v.x + transform.a12 * v.y + transform.a13 * v.z,
+                        .y = transform.a21 * v.x + transform.a22 * v.y + transform.a23 * v.z,
+                        .z = transform.a31 * v.x + transform.a32 * v.y + transform.a33 * v.z };
 }
 
 typedef struct camera_t {
@@ -168,15 +160,12 @@ typedef struct ray_sphere_intersection_t {
     double t;
 } ray_sphere_intersection_t;
 
-enum LIGHT_TYPE {
-    LIGHT_TYPE_POINT,
-    LIGHT_TYPE_DIRECTIONAL,
-    LIGHT_TYPE_AMBIENT
-};
+enum LIGHT_TYPE { LIGHT_TYPE_POINT, LIGHT_TYPE_DIRECTIONAL, LIGHT_TYPE_AMBIENT };
 
 typedef struct light_t {
     int type;
     double intensity;
+
     union {
         vector3_t position;
         vector3_t direction;
@@ -190,15 +179,29 @@ static const double VIEWPORT_WIDTH = 1;
 static const double VIEWPORT_HEIGHT = 1;
 static const double VIEWPORT_CAMERA_DISTANCE = 1;
 
-#define BACKGROUND_COLOR \
+#define BACKGROUND_COLOR                                                                       \
     (color_t) { 0, 0, 0 }
 
-static sphere_t spheres[] = {
-    [0] = { .center = { 0, -1, 3 }, .radius = 1, .color = { 1, 0, 0 }, .specular = 500, .reflective = 0.2f },
-    [1] = { .center = { 2, 0, 4 }, .radius = 1, .color = { 0, 0, 1 }, .specular = 500, .reflective = 0.3f },
-    [2] = { .center = { -2, 0, 4 }, .radius = 1, .color = { 0, 1, 0 }, .specular = 10, .reflective = 0.4f },
-    [3] = { .center = { 0, -5001, 0 }, .radius = 5000, .color = { 1, 1, 0 }, .specular = 1000, .reflective = 0.5f }
-};
+static sphere_t spheres[] = { [0] = { .center = { 0, -1, 3 },
+                                      .radius = 1,
+                                      .color = { 1, 0, 0 },
+                                      .specular = 500,
+                                      .reflective = 0.2f },
+                              [1] = { .center = { 2, 0, 4 },
+                                      .radius = 1,
+                                      .color = { 0, 0, 1 },
+                                      .specular = 500,
+                                      .reflective = 0.3f },
+                              [2] = { .center = { -2, 0, 4 },
+                                      .radius = 1,
+                                      .color = { 0, 1, 0 },
+                                      .specular = 10,
+                                      .reflective = 0.4f },
+                              [3] = { .center = { 0, -5001, 0 },
+                                      .radius = 5000,
+                                      .color = { 1, 1, 0 },
+                                      .specular = 1000,
+                                      .reflective = 0.5f } };
 #define SPHERES_SIZE (sizeof(spheres) / sizeof(sphere_t))
 
 static light_t lights[] = {
@@ -230,12 +233,14 @@ ray_sphere_intersection_t closest_intersection(ray_t ray, double t_min, double t
     sphere_t const *closest_sphere = 0;
     for (size_t i = 0; i < SPHERES_SIZE; ++i) {
         vector2_t intersection_parameters = intersect_ray_sphere(ray, spheres[i]);
-        if (t_min <= intersection_parameters.x && intersection_parameters.x <= t_max && intersection_parameters.x < closest_t) {
+        if (t_min <= intersection_parameters.x && intersection_parameters.x <= t_max &&
+            intersection_parameters.x < closest_t) {
             closest_t = intersection_parameters.x;
             closest_sphere = spheres + i;
         }
 
-        if (t_min <= intersection_parameters.y && intersection_parameters.y <= t_max && intersection_parameters.y < closest_t) {
+        if (t_min <= intersection_parameters.y && intersection_parameters.y <= t_max &&
+            intersection_parameters.y < closest_t) {
             closest_t = intersection_parameters.y;
             closest_sphere = spheres + i;
         }
@@ -257,7 +262,10 @@ bool is_shadowed(ray_t ray, double t_min, double t_max) {
     return false;
 }
 
-double compute_lighting(vector3_t contact_point, vector3_t normal, vector3_t contact_point_to_camera, double specular_exponent) {
+double compute_lighting(
+    vector3_t contact_point, vector3_t normal, vector3_t contact_point_to_camera,
+    double specular_exponent
+) {
     double intensity = 0.0;
     for (size_t k = 0; k < LIGHTS_SIZE; ++k) {
         const light_t *light = lights + k;
@@ -279,16 +287,19 @@ double compute_lighting(vector3_t contact_point, vector3_t normal, vector3_t con
             //     continue;
             // }
 
-            if (is_shadowed((ray_t){ .position = contact_point, .direction = light_vector },
-                            0.001,
-                            t_max)) {
+            if (is_shadowed(
+                    (ray_t){ .position = contact_point, .direction = light_vector },
+                    0.001,
+                    t_max
+                )) {
                 continue;
             }
 
             // diffuse
             double dot = vector3_dot(light_vector, normal);
             if (dot > 0) {
-                intensity += light->intensity * dot / (vector3_length(light_vector) * vector3_length(normal));
+                intensity += light->intensity * dot /
+                             (vector3_length(light_vector) * vector3_length(normal));
             }
 
             // specular
@@ -298,7 +309,10 @@ double compute_lighting(vector3_t contact_point, vector3_t normal, vector3_t con
             vector3_t reflection = vector3_reflect(light_vector, normal);
             double dot2 = vector3_dot(reflection, contact_point_to_camera);
             if (dot2 > 0) {
-                intensity += light->intensity * pow(dot2 / (vector3_length(reflection) * vector3_length(contact_point_to_camera)), specular_exponent);
+                intensity +=
+                    light->intensity * pow(dot2 / (vector3_length(reflection) *
+                                                   vector3_length(contact_point_to_camera)),
+                                           specular_exponent);
             }
         }
     }
@@ -312,15 +326,17 @@ color_t trace_ray(ray_t ray, double t_min, double t_max, int depth) {
         return BACKGROUND_COLOR;
     }
 
-    vector3_t contact_point = vector3_add(ray.position, vector3_scale(ray.direction, intersection.t));
-    vector3_t normal = vector3_normalize(vector3_subtract(contact_point, intersection.sphere->center));
+    vector3_t contact_point =
+        vector3_add(ray.position, vector3_scale(ray.direction, intersection.t));
+    vector3_t normal =
+        vector3_normalize(vector3_subtract(contact_point, intersection.sphere->center));
 
-    double lighting = compute_lighting(contact_point, normal, vector3_negate(ray.direction), intersection.sphere->specular);
-    color_t local_color = {
-        .r = clamp((intersection.sphere->color.r * lighting), 0, 1),
-        .g = clamp((intersection.sphere->color.g * lighting), 0, 1),
-        .b = clamp((intersection.sphere->color.b * lighting), 0, 1)
-    };
+    double lighting = compute_lighting(
+        contact_point, normal, vector3_negate(ray.direction), intersection.sphere->specular
+    );
+    color_t local_color = { .r = clamp((intersection.sphere->color.r * lighting), 0, 1),
+                            .g = clamp((intersection.sphere->color.g * lighting), 0, 1),
+                            .b = clamp((intersection.sphere->color.b * lighting), 0, 1) };
 
     double r = intersection.sphere->reflective;
     if (depth <= 0 || r <= 0) {
@@ -328,28 +344,27 @@ color_t trace_ray(ray_t ray, double t_min, double t_max, int depth) {
     }
 
     vector3_t reflected_direction = vector3_reflect(vector3_negate(ray.direction), normal);
-    color_t reflected_color = trace_ray((ray_t){ .position = contact_point, .direction = reflected_direction }, 0.001, INFINITY, depth - 1);
+    color_t reflected_color = trace_ray(
+        (ray_t){ .position = contact_point, .direction = reflected_direction },
+        0.001,
+        INFINITY,
+        depth - 1
+    );
 
-    return (color_t){
-        .r = lerp(local_color.r, reflected_color.r, r),
-        .g = lerp(local_color.g, reflected_color.g, r),
-        .b = lerp(local_color.b, reflected_color.b, r)
-    };
+    return (color_t){ .r = lerp(local_color.r, reflected_color.r, r),
+                      .g = lerp(local_color.g, reflected_color.g, r),
+                      .b = lerp(local_color.b, reflected_color.b, r) };
 }
 
 vector3_t canvas_to_viewport(int canvas_x, int canvas_y) {
-    return (vector3_t){
-        .x = (double)canvas_x * VIEWPORT_WIDTH / CANVAS_WIDTH,
-        .y = (double)canvas_y * VIEWPORT_HEIGHT / CANVAS_HEIGHT,
-        .z = VIEWPORT_CAMERA_DISTANCE
-    };
+    return (vector3_t){ .x = (double)canvas_x * VIEWPORT_WIDTH / CANVAS_WIDTH,
+                        .y = (double)canvas_y * VIEWPORT_HEIGHT / CANVAS_HEIGHT,
+                        .z = VIEWPORT_CAMERA_DISTANCE };
 }
 
 vector2_t canvas_to_screen(int canvas_x, int canvas_y) {
-    return (vector2_t){
-        .x = CANVAS_WIDTH / 2.0 + (double)canvas_x,
-        .y = CANVAS_HEIGHT / 2.0 - (double)canvas_y
-    };
+    return (vector2_t){ .x = CANVAS_WIDTH / 2.0 + (double)canvas_x,
+                        .y = CANVAS_HEIGHT / 2.0 - (double)canvas_y };
 }
 
 void put_pixel(int canvas_x, int canvas_y, color_t color) {
@@ -357,18 +372,20 @@ void put_pixel(int canvas_x, int canvas_y, color_t color) {
     DrawPixel(
         (int)screen_coord.x,
         (int)screen_coord.y,
-        (Color){
-            .r = (uint8_t)round(color.r * 255),
-            .g = (uint8_t)round(color.g * 255),
-            .b = (uint8_t)round(color.b * 255),
-            .a = 255 });
+        (Color){ .r = (uint8_t)round(color.r * 255),
+                 .g = (uint8_t)round(color.g * 255),
+                 .b = (uint8_t)round(color.b * 255),
+                 .a = 255 }
+    );
 }
 
 RenderTexture target;
 
 void draw() {
     BeginDrawing();
-    Rectangle sourceRec = { 0.0, 0.0, (float)target.texture.width, -(float)target.texture.height };
+    Rectangle sourceRec = {
+        0.0, 0.0, (float)target.texture.width, -(float)target.texture.height
+    };
     Rectangle destRec = { 0.0, 0.0, CANVAS_WIDTH, CANVAS_HEIGHT };
     DrawTexturePro(target.texture, sourceRec, destRec, (Vector2){ 0, 0 }, 0, WHITE);
     EndDrawing();
@@ -383,10 +400,10 @@ int main() {
 
     SetTargetFPS(60);
 
-    camera_t camera = {
-        .position = { 3, 4, -2 },
-        .rotation = matrix3_multiply(matrix3_y_rotation_from_angle(-M_PI / 6), matrix3_x_rotation_from_angle(M_PI / 6))
-    };
+    camera_t camera = { .position = { 0, 0, 0 },
+                        .rotation = matrix3_multiply(
+                            matrix3_y_rotation_from_angle(0), matrix3_x_rotation_from_angle(0)
+                        ) };
     target = LoadRenderTexture(CANVAS_WIDTH, CANVAS_HEIGHT);
 
     BeginTextureMode(target);
@@ -394,7 +411,8 @@ int main() {
     for (int x = -CANVAS_WIDTH / 2; x <= CANVAS_WIDTH / 2; ++x) {
         for (int y = -CANVAS_HEIGHT / 2; y <= CANVAS_HEIGHT / 2; ++y) {
             ray_t ray = { .position = camera.position,
-                          .direction = vector3_transform(canvas_to_viewport(x, y), camera.rotation) };
+                          .direction =
+                              vector3_transform(canvas_to_viewport(x, y), camera.rotation) };
             color_t color = trace_ray(ray, 1, INFINITY, 3);
             put_pixel(x, y, color);
         }
